0888f0682cdacae0fb7c3616dd127c5803d91375
Colm Kennedy <colmkennedy@Colms-iMac.local>
2012-10-21 16:32:06 +0100

    gasta

---
+++ product/product/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  product
+//
+//  Created by Colm Kennedy on 21/10/2012.
+//  Copyright (c) 2012 Colm Kennedy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
